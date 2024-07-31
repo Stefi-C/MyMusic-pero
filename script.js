b8b5a09b4b0204225f823628f39069e6dcee22f8
Stefi-C <cristinistif000@icloud.com>
2024-07-31 13:45:38 +0200

    ...

--- script.js
+++ script.js
@@ -292,7 +292,7 @@ listVideo.forEach(video => {
         })
       })
       const updateImage = (i) => {
-        let path = `assets/gallery-img${i+1}.jpg`;
+        let path = `./gallery-img${i+1}.jpg`;
         largeImage.src = path;
         imageName.innerHTML = path;
         imageIndex.innerHTML = `0${i+1}`;
